8441b69a674d85f113479c968577be53cd0edc0c
Erik M. Bray <embray@stsci.edu>
2013-11-18 18:36:31 -0500

    This should have been removed

--- github/labels_on_pull_requests/GitHub_Labels_on_Pull_Requests.user.js
+++ github/labels_on_pull_requests/GitHub_Labels_on_Pull_Requests.user.js
@@ -47,7 +47,7 @@ function ghAppendDiscussionLabels() {
   var context = {labels: ghRepoLabels, issueLabels: ghIssueLabels, dataUrl: ghIssuePostUrl,
                issue: ghPRNumber, csrf_token: ghCSRFToken};
 
-  $("div.discussion-sidebar").append("<hr></hr>").append(ghDiscussionLabelsTemplate(context, options));
+  $("div.discussion-sidebar").append("<hr></hr>").append(ghDiscussionLabelsTemplate(context));
 }
 
 GM_xmlhttpRequest({
